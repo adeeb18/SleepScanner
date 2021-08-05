a4d2cae26273c4076c44124540d21a5afbd831cd
Adeeb Rashid <adeeb.rashid@ufl.edu>
2021-08-04 20:48:14 -0400

    Updates to taskbar UI

--- app/src/main/java/com/example/sleepsurvey/DashboardActivity.java
+++ app/src/main/java/com/example/sleepsurvey/DashboardActivity.java
@@ -1,9 +1,15 @@
 package com.example.sleepsurvey;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.navigation.NavController;
+import androidx.navigation.Navigation;
+import androidx.navigation.ui.NavigationUI;
+
 import android.view.View;
 import android.os.Bundle;
 import android.content.Intent;
+
+import com.google.android.material.navigation.NavigationView;
 
 public class DashboardActivity extends AppCompatActivity {
 
@@ -11,6 +17,13 @@ public class DashboardActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_dashboard);
+
+
+        NavController nc = Navigation.findNavController(this,R.id.fragmentContainerView);
+        NavigationView nv = findViewById(R.id.navigationView);
+        NavigationUI.setupWithNavController(nv,nc);
+
+
     }
 
     public void launchSensors(View v) {
